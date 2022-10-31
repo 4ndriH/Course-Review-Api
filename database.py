cb398af2659e6059dd285c09478aae4bd613aad2
EvilBabyDemon <lteufelbe@ethz.ch>
2022-10-31 16:48:28 +0100

    accidentally removed it

--- database.py
+++ database.py
@@ -44,6 +44,12 @@ def getAllReviews():
     cnx.close()
     return json.dumps((r[0] if data else None) if False else data)
 
+def getStatsReviews():
+    cnx = sqlite3.connect(path)
+    cursor = cnx.execute("SELECT COUNT(DISTINCT CourseNumber) AS percourse, COUNT(*) AS total FROM CourseReviews WHERE Verified=1")
+    data = [dict((cursor.description[i][0], value) for i, value in enumerate(row)) for row in cursor.fetchall()]
+    cnx.close()
+    return json.dumps((r[0] if data else None) if False else data)
 
 
 def removeCourseReviews(CourseNumber, nethz):
