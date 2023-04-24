b63c89b36dd443824fbe6d11085d27c91d148f32
EvilBabyDemon <lteufelbe@ethz.ch>
2023-04-24 14:24:26 +0200

    return course number and name for allCourses

--- database.py
+++ database.py
@@ -62,7 +62,7 @@ def getPublishedReviewStats():
 
 def getAllCoursesWithReviews():
     cnx = sqlite3.connect(path)
-    cursor = cnx.execute("SELECT CourseNumber FROM CourseReviews WHERE VerificationStatus=1 GROUP BY CourseNumber HAVING MAX(Date) ORDER BY Date")
+    cursor = cnx.execute("SELECT c.CourseNumber, CourseName FROM (SELECT CourseNumber FROM CourseReviews WHERE VerificationStatus= 1 GROUP BY CourseNumber HAVING MAX(Date) ORDER BY Date DESC) cn INNER JOIN Courses c ON cn.CourseNumber = c.CourseNumber")
     data = [dict((cursor.description[i][0], value) for i, value in enumerate(row)) for row in cursor.fetchall()]
     cnx.close()
     return json.dumps((r[0] if data else None) if False else data)
