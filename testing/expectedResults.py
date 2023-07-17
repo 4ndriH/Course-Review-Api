928838460b6a7531db3af43f8ded78031c9e3114
4ndriH <45722342+4ndriH@users.noreply.github.com>
2023-07-17 12:34:42 +0200

    added files for automated testing
    this is still wip and requires some trial and error

---
+++ testing/expectedResults.py
@@ -0,0 +1,17 @@
+home = {"Playing with": "Duckies"}
+authentication = {"Authentication": "Successful"}
+latestReviews = [{"CourseNumber": "00-000-00X", "CourseName": "CourseReview"}, {"CourseNumber": "00-000-01X", "CourseName": "Hardwell"}, {"CourseNumber": "00-000-02X", "CourseName": "Timmy Trumpet"}, {"CourseNumber": "00-000-03X", "CourseName": "Will Sparks"}, {"CourseNumber": "00-000-04X", "CourseName": "Gordo"}, {"CourseNumber": "00-000-05X", "CourseName": "Yellow Claw"}, {"CourseNumber": "00-000-06X", "CourseName": "TRYM"}, {"CourseNumber": "00-000-07X", "CourseName": "Sub Zero Project"}, {"CourseNumber": "00-000-08X", "CourseName": "Warface"}, {"CourseNumber": "00-000-09X", "CourseName": "Steve Aoki"}]
+allReviews = [{'CourseNumber': '00-000-00X', 'CourseName': 'CourseReview'}, {'CourseNumber': '00-000-01X', 'CourseName': 'Hardwell'}, {'CourseNumber': '00-000-02X', 'CourseName': 'Timmy Trumpet'}, {'CourseNumber': '00-000-03X', 'CourseName': 'Will Sparks'}, {'CourseNumber': '00-000-04X', 'CourseName': 'Gordo'}, {'CourseNumber': '00-000-05X', 'CourseName': 'Yellow Claw'}, {'CourseNumber': '00-000-06X', 'CourseName': 'TRYM'}, {'CourseNumber': '00-000-07X', 'CourseName': 'Sub Zero Project'}, {'CourseNumber': '00-000-08X', 'CourseName': 'Warface'}, {'CourseNumber': '00-000-09X', 'CourseName': 'Steve Aoki'}, {'CourseNumber': '99-999-99X', 'CourseName': 'Blood For Mercy'}]
+stats = [{'percourse': 11, 'total': 11}]
+course = [{'Review': 'Lorem gipsum dolor sit amet', 'Semester': 'HS00'}]
+rating = [{'AVG(Recommended)': 5.0, 'AVG(Interesting)': 4.0, 'AVG(Difficulty)': 3.0, 'AVG(Effort)': 2.0, 'AVG(Resources)': 1.0}]
+userStuff = [{'CourseNumber': '00-000-00X', 'Review': 'Lorem ipsum dolor sit amet', 'VerificationStatus': 1, 'Semester': 'HS00', 'Recommended': 5, 'Interesting': 4, 'Difficulty': 3, 'Effort': 2, 'Resources': 1}, {'CourseNumber': '00-000-01X', 'Review': 'consectetur adipiscing elit', 'VerificationStatus': 
+1, 'Semester': 'HS00', 'Recommended': 5, 'Interesting': 4, 'Difficulty': 3, 'Effort': 2, 'Resources': 1}, {'CourseNumber': '00-000-02X', 'Review': 'In ut tempor augue', 'VerificationStatus': 1, 'Semester': 'HS00', 'Recommended': 5, 'Interesting': 4, 'Difficulty': 3, 'Effort': 2, 'Resources': 1}, {'CourseNumber': '00-000-03X', 'Review': 'Vivamus scelerisque mollis viverra', 'VerificationStatus': 1, 'Semester': 'HS00', 'Recommended': 5, 'Interesting': 4, 'Difficulty': 3, 'Effort': 2, 'Resources': 1}, {'CourseNumber': '00-000-04X', 'Review': 'Quisque eu felis interdum', 'VerificationStatus': 1, 'Semester': 'HS00', 'Recommended': 5, 'Interesting': 4, 'Difficulty': 3, 'Effort': 2, 'Resources': 1}]
+insertReview = [{'Review': 'beep boop', 'Semester': None}]
+insertRating = [{'AVG(Recommended)': 1.0, 'AVG(Interesting)': 2.0, 'AVG(Difficulty)': 3.0, 'AVG(Effort)': 4.0, 'AVG(Resources)': 5.0}]
+updateReview = [{'Review': 'updoot', 'Semester': None}]
+updateRating = [{'AVG(Recommended)': 2.0, 'AVG(Interesting)': 2.0, 'AVG(Difficulty)': 3.0, 'AVG(Effort)': 4.0, 'AVG(Resources)': 5.0}]
+insertSemester = [{'Review': 'updoot', 'Semester': 'FS99'}]
+removeSemester = [{'Review': 'updoot', 'Semester': None}]
+removeReview = []
+removeRating = [{'AVG(Recommended)': None, 'AVG(Interesting)': 2.0, 'AVG(Difficulty)': 3.0, 'AVG(Effort)': 4.0, 'AVG(Resources)': 5.0}]
